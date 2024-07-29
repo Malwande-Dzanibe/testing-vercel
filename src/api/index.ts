003f5fc00ce5ac5085c72df61d529f9c42f81757
Malwande-Dzanibe <malwandedza@gmail.com>
2024-07-30 00:03:55 +0200

    auth-with-next-frontend-backend

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 
+import MessageResponse from "../interfaces/MessageResponse";
 import user from "./user";
 import authRoute from "./authRoute";
 import tweetRoute from "./tweetRoute";
 
 const router = express.Router();
+
+router.get<{}, MessageResponse>("/", (req, res) => {
+  res.json({
+    message: "API - 👋🌎🌍🌏",
+  });
+});
 
 router.use("/user", user);
 router.use("/authenticate", authRoute);
