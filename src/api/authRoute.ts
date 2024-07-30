c98bff94a4de87c7137b0431b8001470d53503a0
Malwande-Dzanibe <malwandedza@gmail.com>
2024-07-30 21:49:16 +0200

    auth-with-next-frontend-backend

--- src/api/authRoute.ts
+++ src/api/authRoute.ts
@@ -13,6 +13,23 @@ const generateJWT = (id: string) => {
 };
 
 router.post("/", async (req, res) => {
+  // res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   const emailToken = req.body.token.emailToken;
   const email = req.body.userEmail;
 
