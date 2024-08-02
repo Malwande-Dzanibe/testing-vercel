f5781f8bfcec16297b3b0a845f26138c3d4105a4
Malwande-Dzanibe <malwandedza@gmail.com>
2024-08-02 18:38:57 +0200

    auth-with-next-frontend-backend

--- src/api/user.ts
+++ src/api/user.ts
@@ -80,7 +80,7 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     console.log({ error });
 
-    res.status(500).json({ fromPrisma: process.env.DATABASE_URL });
+    res.status(500).json({ error: await prisma.post.findMany() });
   }
 });
 
@@ -146,7 +146,7 @@ router.get("/tweets", async (req, res) => {
     res.status(200).json(tweets);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ fromPrisma: process.env.DATABASE_URL });
+    res.status(500).json({ error: await prisma.post.findMany() });
   }
 });
 
